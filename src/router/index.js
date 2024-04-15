018970f4035ba768c8a58a9e63ba24891915bfd5
victor <victoroliveira34vv@gmail.com>
2024-04-15 14:34:13 -0300

    avaliação do sonarqube

--- src/router/index.js
+++ src/router/index.js
@@ -11,11 +11,14 @@ import routes from './routes'
  * with the Router instance.
  */
 
-export default route(function (/* { store, ssrContext } */) {
-  const createHistory = process.env.SERVER
-    ? createMemoryHistory
-    : (process.env.VUE_ROUTER_MODE === 'history' ? createWebHistory : createWebHashHistory)
-
+export default route(function () {
+  let historyType;
+  if (process.env.SERVER) {
+    historyType = createMemoryHistory;
+  } else {
+    historyType = process.env.VUE_ROUTER_MODE === 'history' ? createWebHistory : createWebHashHistory;
+  }
+  const createHistory = historyType;
   const Router = createRouter({
     scrollBehavior: () => ({ left: 0, top: 0 }),
     routes,
